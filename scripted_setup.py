0b746993d5569ff31263ef3a0026229b583d8fc2
Vanessa Moss <moss@marsfield-visitor-106-140.pool.csiro.au>
2019-06-27 20:32:23 +1000

    Add ARTS setup as scripted with input from L.C. Oostrum

--- scripted_setup.py
+++ scripted_setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-# Automated Apertif setup (scripted_setup.py) - imaging version
+# Automated Apertif setup (scripted_setup.py) - now with ARTS?
 # V.A. Moss 05/05/2019 (vmoss.astro@gmail.com)
-__author__ = "V.A. Moss"
-__date__ = "$26-jun-2019 17:00:00$"
-__version__ = "1.1"
+__author__ = "V.A. Moss & L.C. Oostrum"
+__date__ = "$27-jun-2019 17:00:00$"
+__version__ = "1.2"
 
 import os
 import sys
@@ -16,7 +16,6 @@ com_desp = '/home/apertif/UniBoard_FP7/RadioHDL/trunk/applications/apertif/commi
 radio_hdl = '/home/apertif/UniBoard_FP7/RadioHDL/trunk/'
 ub7_bad = False
 ub5_bad = False
-# executor = False
 
 # Get software
 try:
@@ -25,11 +24,6 @@ except:
 	sw = json.load(open('sw.json'))
 print(sw)
 
-# Load the software versions
-sw_version_corr = sw['imaging']['sw_corr']
-sw_version_rt = sw['imaging']['sw_rt']
-sw_version_dw = sw['imaging']['sw_dw']
-
 # Argument parsing
 parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
 parser.add_argument('-t', '--telescopes',
@@ -37,14 +31,24 @@ parser.add_argument('-t', '--telescopes',
 	help='Specify which telescopes to include (default: %(default)s)')
 parser.add_argument('-m', '--mode',
 	default='warm',
+	choices=['cold', 'warm'],
 	help='Specify whether to warm start or cold start (default: %(default)s)')
 parser.add_argument('-f', '--firmware',
 	default='main',
-	help='Specify whether to use main or executor setup (default: %(default)s)')
+	choices=['main', 'executor'],
+	help='Specify whether to use main/executor setup (default: %(default)s)')
 parser.add_argument('-d', '--dryrun',
 	default=False,
 	action='store_true',
 	help='Specify whether to execute a dry-run (default: %(default)s)')
+parser.add_argument('-s', '--science',
+	default='imaging',
+	choices=['imaging', 'sc1','sc4'],
+	help='Specify which science mode of imaging/sc1/sc4 (default: %(default)s)')
+parser.add_argument('-a', '--artsmode',
+	default='tab',
+	choices=['tab', 'iab'],
+	help='Specify which ARTS science mode of tab/iab (default: %(default)s)')
 args = parser.parse_args()
 
 # Parse the arguments
@@ -73,6 +77,23 @@ else:
 
 # Specify telescopes
 rts = args.telescopes
+smode = args.science
+
+# Main.py names
+if smode == 'imaging':
+	fwmode = 'apertif-dev'
+elif smode == 'sc4':
+	if args.artsmode == 'iab':
+		fwmode = 'arts_sc4-iab'
+	elif args.artmode == 'tab':
+		fwmode = 'arts_sc4-tab'
+elif smode == 'sc1':
+	fwmode = 'arts_sc1'
+
+# Load the software versions
+sw_version_corr = sw[args.science]['sw_corr']
+sw_version_rt = sw[args.science]['sw_rt']
+sw_version_dw = sw[args.science]['sw_dw']
 
 if dryrun:
 	print('\n################################################################################\nRUNNING IN DRYRUN MODE!!!\n################################################################################')
@@ -85,8 +106,20 @@ print(cmd)
 if not dryrun:
 	os.system(cmd)
 
-# Turn off datawriter+ATDB
-cmd = """ ssh -t apertif@lcu-head.apertif "clush -w wcudata1 'supervisorctl -p 123 -u user stop WCUData: atdb_services_executors:'" """
+# Turn off datawriter+ATDB (depending on science mode)
+if smode == 'imaging':
+	# ATDB executors no longer stopped here
+	cmd = """ ssh -t apertif@lcu-head.apertif "clush -w wcudata1 'supervisorctl -p 123 -u user stop WCUData:'" """
+elif smode == 'sc1':
+	cmd = """ ssh -t apertif@lcu-head.apertif "clush -w arts-sc1 'supervisorctl -p 123 -u user stop all'" """
+elif smode == 'sc4':
+	# Master node
+	cmd = """ ssh arts@arts041.apertif "sudo systemctl stop arts-survey.service" """
+	print(cmd)
+	if not dryrun:
+		os.system(cmd)
+	# cluster nodes
+	cmd = """ ssh arts@arts041.apertif "ansible -f 10 artscluster_nodes -a 'sudo systemctl stop arts-nodes.service'" """
 print(cmd)
 if not dryrun:
 	os.system(cmd)
@@ -97,39 +130,48 @@ print(cmd)
 if not dryrun:
 	os.system(cmd)
 
+# Install software on telescopes (pass 1)
 cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-LCU-RT -s LCU-RT -g lcu-rt[2-13] -a" <<< "y" """ % (sw_version_rt)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
+# Install software on correlator (pass 1)
 cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-CCU-Corr -s CCU-Corr -g ccu-corr -a" <<< "y" """ % (sw_version_corr)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
-cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-DataWriter -s DataWriter -g wcudata[1] -a" <<< "y" """ % (sw_version_dw)
+# Install software on datawriter/ARTS depending on science mode (pass 1)
+if smode == 'imaging':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-DataWriter -s DataWriter -g wcudata[1] -a" <<< "y" """ % (sw_version_dw)
+elif smode == 'sc1':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-ARTS -s ARTS -g arts -a" <<< "y" """ % (sw_version_dw)
+elif smode == 'sc4':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-ARTSCLUSTER -s ARTSCLUSTER -g artscluster -a" <<< "y" """ % (sw_version_dw)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
+
+# UNIBOARD LAND BEGINS HERE
 if warm_start:
-
 	if executor:
-
-		cmd = """ ssh -t apertif@ccu-corr.apertif "executor --run warm --ccu ccu-corr --telescopes %s --application apertif-dev" """ % (rts)
+		cmd = """ ssh -t apertif@ccu-corr.apertif "executor --run warm --ccu ccu-corr --telescopes %s --application %s" """ % (rts,fwmode)
 	else:
-		cmd = """ ssh -t apertif@ccu-corr.apertif  "python %s/main.py --app apertif-dev --tel %s --unb 0:15 --pol 0:1 --rerun" """ % (com_desp,rts)
+		cmd = """ ssh -t apertif@ccu-corr.apertif  "python %s/main.py --app %s --tel %s --unb 0:15 --pol 0:1 --rerun" """ % (com_desp,fwmode,rts)
 else:
 	if executor:
-		cmd = """ ssh -t apertif@ccu-corr.apertif "executor --run cold --ccu ccu-corr --telescopes %s --application apertif-dev" """ % (rts)
+		cmd = """ ssh -t apertif@ccu-corr.apertif "executor --run cold --ccu ccu-corr --telescopes %s --application %s" """ % (rts,fwmode)
 	else:
-		cmd = """ ssh -t apertif@ccu-corr.apertif  "python %s/main.py --app apertif-dev --tel %s --unb 0:15 --pol 0:1" """ % (com_desp,rts)
+		cmd = """ ssh -t apertif@ccu-corr.apertif  "python %s/main.py --app %s --tel %s --unb 0:15 --pol 0:1" """ % (com_desp,fwmode,rts)
 print(cmd)
 if not dryrun:
 	os.system(cmd)
 
-if ub7_bad or ub5_bad:
 
+# These can go long term, only here temporarily while issues are seen
+if ub7_bad or ub5_bad:
 	# Read layout of firmware
 	cmd = """ ssh -t apertif@ccu-corr.apertif 'python /home/apertif/UniBoard_FP7/UniBoard/trunk/Software/python/peripherals/util_system_info.py --unb 0:15 --fn 0:3 --bn 0:3 -n 4' """
 	print(cmd)	
@@ -142,47 +184,61 @@ if ub7_bad or ub5_bad:
 				print(cmd)	
 				if not dryrun:
 						os.system(cmd)
-
 		if ub5_bad:
 				# Disable link                                                                                                                                                                                                         
 				cmd = """ ssh -t apertif@ccu-corr.apertif 'python $UPE/peripherals/util_dp_bsn_aligner.py --unb 5 --bn 1 -n 2 -r 0 -s INPUT' """
 				print(cmd)
 				if not dryrun:
 						os.system(cmd)
-
 	cmd = """ ssh -t apertif@ccu-corr.apertif '%s/applications/apertif/commissioning/central_status.sh apertif-dev %s 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 postcheck 0,1' """ % (radio_hdl,rts)
 	print(cmd)	
 	if not dryrun:
 		os.system(cmd)
 
+# Install software on telescopes (pass 2)
 cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-LCU-RT -s LCU-RT -g lcu-rt[2-13] -a" <<< "y" """ % (sw_version_rt)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
+# Install software on correlator (pass 2)
 cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-CCU-Corr -s CCU-Corr -g ccu-corr -a" <<< "y" """ % (sw_version_corr)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
-cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-DataWriter -s DataWriter -g wcudata[1] -a" <<< "y" """ % (sw_version_dw)
+# Install software on datawriter/ARTS depending on science mode (pass 2)
+if smode == 'imaging':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-DataWriter -s DataWriter -g wcudata[1] -a" <<< "y" """ % (sw_version_dw)
+elif smode == 'sc1':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-ARTS -s ARTS -g arts -a" <<< "y" """ % (sw_version_dw)
+elif smode == 'sc4':
+	cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -b %s-ARTSCLUSTER -s ARTSCLUSTER -g artscluster -a" <<< "y" """ % (sw_version_dw)
 print(cmd)
 if not dryrun:
 	os.system("bash -c '{}'".format(cmd))
 
+# Check installation
 cmd = """ ssh -t apertif@lcu-head.apertif "Apertif_install.sh -c -g all" """
 print(cmd)
 if not dryrun:
 	os.system(cmd)
 
+# Check that it is running
 cmd = """ ssh -t apertif@lcu-head.apertif "clush --all 'supervisorctl -p 123 -u user status' | sort" """
 print(cmd)
 if not dryrun:
 	os.system(cmd)
 
-cmd = """ ssh -t apertif@wcudata1.apertif "./packet_rate.sh" """
+# If imaging mode, check packet rate
+if smode == 'imaging':
+	cmd = """ ssh -t apertif@wcudata1.apertif "./packet_rate.sh" """
+elif smode == 'sc1':
+	cmd = """ ssh arts@arts0.apertif "python scripts/packet_rate.py" """
+elif smode == 'sc4':
+	cmd = """ ssh arts@arts001.apertif "packet_rate" """
 print(cmd)
-if not dryrun:
+if not dryrun and smode == 'imaging':
 	os.system(cmd)
 
 print('################################################################################\n')
