0581bd1922717297744956dba3663f15338bbe73
Vanessa Moss <rhobslein63@gmail.com>
2019-09-11 11:43:33 +0000

    Add TID as an argument

--- check_bw_status.py
+++ check_bw_status.py
@@ -6,11 +6,19 @@ __author__ = "V.A. Moss"
 __date__ = "$10-may-2019 17:00:00$"
 __version__ = "1.0"
 
+#change:R Blaauw 11-sep-2019 changed script to accept tid as argument
+
 import os
 import sys
 
+try:
+    tid = sys.argv[1]
+except IndexError:
+    print('Please give beamweight id as argument')
+    sys.exit()
+
 current = False
-tid = 90510001
+#tid = 90910001
 count = False
 
 telescopes = ['2','3','4','5','6','7','8','9','a','b','c','d']
@@ -28,13 +36,13 @@ for i in range(0,len(telescopes)):
 	elif tid != None and count == False:
 		print('#########################################\nCurrent status for RT%s (TID = %s):' % (scope.upper(),tid))
 		cmd = 'ssh -t apertif@lcu-rt%s.apertif "ls -ltr /opt/apertif/var/run/calibration/*%s*"' % (scope,tid)
-		results = os.system(cmd)
+                results = os.system(cmd)
 		print('#########################################\n')
 
 	elif tid != None and count == True:
 		print('#########################################\nCurrent files for RT%s (TID = %s):' % (scope.upper(),tid))
 		cmd = 'ssh -t apertif@lcu-rt%s.apertif "ls -ltr /opt/apertif/var/run/calibration/*%s* | wc -l"' % (scope,tid)
-		results = os.system(cmd)
+                results = os.system(cmd)
 		print('#########################################\n')
 
 
